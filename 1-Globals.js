ea47ce7b540226c5ce6f864ee59fcde8a204166c
Abhijeet-kadance <Abhijeet.kadance.96@gmail.com>
2022-01-03 10:37:48 +0530

    Global variables

---
+++ 1-Globals.js
@@ -0,0 +1,13 @@
+// __dirname  = paath to current directory
+// __filename = file name
+// require    = function to use modules (CommanJS)
+// module     = into about current module (file)
+// process    = info about env where the program is being executed
+
+//console.log(__dirname)
+//console.log(__filename)
+//console.log(require)
+
+// setInterval(()=> {
+//     console.log('hello world')
+// }, 1000)
